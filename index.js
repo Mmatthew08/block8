2cfff49ec0b43ce7c76d214de13fbfd698b22d96
Matthew Eski <metehaneski08@gmail.com>
2023-10-05 11:10:28 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+console.log('Hello from index.js')
+
+
+alert('ALERT!') 
+// Add comments at the end of the line
+
+
+//VARIABLES
+let favoriteNumber 
+console.log(favoriteNumber)
+
+
+
+let myName = 'Matthew'
+console.log(myName)
+myName= 'Metehan' // to change the variable put variable without"let"
+console.log(myName)
+ 
+const gravitiy= 9.8
+ 
+// useCamelCaseInJavascript 
+
+const javascript = 'JavaScript'
+console.log('I enjoy programming in' + javascript +)
+
+let hello='Hello'
+let world='World'
+
+console.log(hello+','+world+'!') // String Concantenation
+
+let mystery='some value' +37
+let mystery2='true' + true + hello
+
+console.log('The datatype of mystery:', typeof mystery )
+console.log('The datatype of mystery2 :' , typeof mystery2)
+console.log(typeof(null+45))
+console.log(typeof 42)
+
+
+//ARITMETHIC OPERATIONS
+
+console.log(1+4/20) 
+console.log(52525%22)
+
+//Logical Operations
+
+console.log(boolean)
+console.log(!boolean)
+console.log(Boolean(boolean && ''))
+
