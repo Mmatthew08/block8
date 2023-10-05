65b2c339e6c74a7163d0a5b90a27d3a6bc483a7d
Matthew Eski <metehaneski08@gmail.com>
2023-10-05 12:54:26 -0500

    With Lock Assingment

---
+++ LockAssingment.js
@@ -0,0 +1,11 @@
+
+// First, I put a message to be shown at the top of the screen
+
+let message= 'YOU ARE THE CHOSEN ONE. HERE IS THE COMBINATION FOR THE LOCK : 10-40-39'
+alert(message)
+//  Then, I put aritmethic operations for the combination of the lock
+
+let number1= (((100/5)*2)-31)*2-8
+let number2= (((4*4*4)-20)*2)-48
+let number3= (5*5*5)-(12*5)-((6+7)*2)
+ 
